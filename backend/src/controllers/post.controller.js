ea814ba3ff7cd24b6138d228f464d578a33e8b56
Jatesh Parikh <jateshparikh97@gmail.com>
2026-02-21 10:26:54 +0530

    add get user posts controller

--- backend/src/controllers/post.controller.js
+++ backend/src/controllers/post.controller.js
@@ -38,3 +38,23 @@ export const getPost = asyncHandler(async (req, res) => {
 
   res.status(200).json({ post });
 });
+
+export const getUserPosts = asyncHandler(async (req, res) => {
+  const { username } = req.params;
+
+  const user = await User.findOne({ username });
+  if (!user) return res.status(404).json({ error: "User not found" });
+
+  const posts = await Post.find({ user: user._id })
+    .sort({ createdAt: -1 })
+    .populate("user", "username firstName lastName profilePicture")
+    .populate({
+      path: "comments",
+      populate: {
+        path: "user",
+        select: "username firstName lastName profilePicture",
+      },
+    });
+
+  res.status(200).json({ posts });
+});
