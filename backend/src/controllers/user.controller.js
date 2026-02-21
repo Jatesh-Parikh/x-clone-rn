32d20416e94bd39c809f739d8b6228437fa7356d
Jatesh Parikh <jateshparikh97@gmail.com>
2026-02-21 15:21:11 +0530

    add sync user controller

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -24,3 +24,31 @@ export const updateProfile = asyncHandler(async (req, res) => {
 
   res.status(200).json({ user });
 });
+
+export const syncUser = asyncHandler(async (req, res) => {
+  const { userId } = getAuth(req);
+
+  // Check if user already exists in mongodb
+  const existingUser = await User.findOne({ clerkId: userId });
+  if (existingUser) {
+    return res
+      .status(200)
+      .json({ user: existingUser, message: "User already exists" });
+  }
+
+  // Create new user from clerk data
+  const clerkUser = await clerkClient.users.getUser(userId);
+
+  const userData = {
+    clerkId: userId,
+    email: clerkUser.emailAddresses[0].emailAddress,
+    firstName: clerkUser.firstName || "",
+    lastName: clerkUser.lastName || "",
+    username: clerkUser.emailAddresses[0].emailAddress.split("@")[0],
+    profilePicture: clerkUser.imageUrl || "",
+  };
+
+  const user = await User.create(userData);
+
+  res.status(201).json({ user, message: "User created successfully" });
+});
